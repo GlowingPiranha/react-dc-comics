c5ec2cdcac4beb43c488b6d16c60e721cc3019e4
GlowingPiranha <ikimus01@gmail.com>
2025-08-25 19:18:15 +0200

    [upd] pulizia e componenti

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
+// * importo le componenti
+import Main from "./components/Main";
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <>
-      <h1>dc comics</h1>
-      <h2>cc-comics</h2>
-    </>
-  );
+  return <Main />;
 }
 
 export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,5 @@
+const Main = () => {
+  return <h1>applicazione dc comics</h1>;
+};
+
+export default Main;

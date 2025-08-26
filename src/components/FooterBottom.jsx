d8301bc8fab47e2b12ec80004a84110ab2b24102
GlowingPiranha <ikimus01@gmail.com>
2025-08-26 19:14:01 +0200

    [upd] skin-index.css

--- src/components/FooterBottom.jsx
+++ src/components/FooterBottom.jsx
@@ -3,16 +3,18 @@ import React from 'react'
 const FooterBottom = () => {
   return (
     <section className="footer-bottom">
-      <button>Sign-up now!</button>
-      <div className="socials">
-        <span>Follow us</span>
-        <ul>
-          <li><img src="./public/img/footer-facebook.png" alt="" /></li>
-          <li><img src="./public/img/footer-Twitter.png" alt="" /></li>
-          <li><img src="./public/img/footer-youtube.png" alt="" /></li>
-          <li><img src="./public/img/footer-pinterest.png" alt="" /></li>
-          <li><img src="./public/img/footer-periscope.png" alt="" /></li>
-        </ul>
+      <div className="container flex-bottom">
+        <button className='signup-btn'>Sign-up now!</button>
+        <div className="footer-socials">
+          <span>Follow us</span>
+          <ul>
+            <li><img src="./public/img/footer-facebook.png" alt="" /></li>
+            <li><img src="./public/img/footer-Twitter.png" alt="" /></li>
+            <li><img src="./public/img/footer-youtube.png" alt="" /></li>
+            <li><img src="./public/img/footer-pinterest.png" alt="" /></li>
+            <li><img src="./public/img/footer-periscope.png" alt="" /></li>
+          </ul>
+        </div>
       </div>
     </section>
   )
--- src/components/FooterLinks.jsx
+++ src/components/FooterLinks.jsx
@@ -2,42 +2,75 @@ import React from 'react'
 
 function FooterLinks() {
   return (
-    <section className="footer-links">
-      <div className="column">
-        <h3>DC Comics</h3>
-        <ul>
-          <li>Characters</li>
-          <li>Comics</li>
-          <li>Movies</li>
-          <li>TV</li>
-          <li>Games</li>
-          <li>Videos</li>
-          <li>News</li>
-        </ul>
+    <div className="footer-links">
+
+      <div className="container">
+        <div className="row">
+          <div className="col-50">
+            <div className="first-col mt-50">
+
+              <div className="column">
+                <h3>DC Comics</h3>
+                <ul>
+                  <li>Characters</li>
+                  <li>Comics</li>
+                  <li>Movies</li>
+                  <li>TV</li>
+                  <li>Games</li>
+                  <li>Videos</li>
+                  <li>News</li>
+                </ul>
+              </div>
+              <div className="column mt-50">
+                <h3>shop</h3>
+                <ul>
+                  <li>shop DC</li>
+                  <li>shop DC Collectibles</li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="second-col mt-50">
+
+              <div className="column">
+                <h3>DC</h3>
+                <ul>
+                  <li>Terms of Use</li>
+                  <li>Privacy Policy (New)</li>
+                  <li>Ad Choices</li>
+                  <li>Advertising</li>
+                  <li>Jobs</li>
+                  <li>Subscriptions</li>
+                  <li>Talent Workshops</li>
+                  <li>CPSC Certificates</li>
+                  <li>Ratings</li>
+                  <li>Shop Help</li>
+                  <li>Contact Us</li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="third-col mt-50">
+
+              <div className="column">
+                <h3>Sites</h3>
+                <ul>
+                  <li>DC</li>
+                  <li>MAD Magazine</li>
+                  <li>DC Kids</li>
+                  <li>DC Universe</li>
+                  <li>DC Power Visa</li>
+                </ul>
+              </div>
+            </div>
+
+          </div>
+          <div className="col-50 footer-dc-logo">
+
+          </div>
+        </div>
       </div>
-      <div className="column">
-        <h3>DC</h3>
-        <ul>
-          <li>Terms of Use</li>
-          <li>Privacy Policy</li>
-          <li>Ad Choices</li>
-          <li>Advertising</li>
-          <li>Jobs</li>
-          <li>Subscriptions</li>
-          <li>Shop Help</li>
-        </ul>
-      </div>
-      <div className="column">
-        <h3>Sites</h3>
-        <ul>
-          <li>DC</li>
-          <li>MAD Magazine</li>
-          <li>DC Kids</li>
-          <li>DC Universe</li>
-          <li>DC Power Visa</li>
-        </ul>
-      </div>
-    </section>
+    </div>
   );
 }
 
